135f80db39f734de1c5a18757bc8c52be2a9ac83
sarkarilock <sarkarilock@gmail.com>
2026-04-04 21:38:07 +0530

    cc

--- main.py
+++ main.py
@@ -17,7 +17,7 @@ app = FastAPI()
 origins = [
     "http://localhost:3000",   
     "http://127.0.0.1:3000",
-    "https://yourdomain.com",  
+    "https://classintel.vercel.app",  
 ]
 
 app.add_middleware(
